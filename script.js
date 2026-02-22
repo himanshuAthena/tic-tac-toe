1b8a3bf4f8913199282703863965f1df60dec694
Himanshu Chouhan <hc979979@gmail.com>
2026-02-22 13:45:17 +0530

    Refactor game logic and improve UI styling for Tic Tac Toe

--- script.js
+++ script.js
@@ -12,28 +12,57 @@ const changeTurn = ()=>{
 }
 
 // Function to check for a win
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
+    let boxes = document.getElementsByClassName('box');
+    let line = document.querySelector(".line");
+
     let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    wins.forEach(e => {
+        if (
+            boxtext[e[0]].innerText !== "" &&
+            boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[1]].innerText === boxtext[e[2]].innerText
+        ) {
+
+            document.querySelector('.info').innerText =
+                boxtext[e[0]].innerText + " Won";
+
+            isgameover = true;
+            document.querySelector('.imgbox img').style.width = "200px";
+
+            // Get first and last winning box
+            let firstBox = boxes[e[0]].getBoundingClientRect();
+            let lastBox = boxes[e[2]].getBoundingClientRect();
+            let container = document.querySelector(".container").getBoundingClientRect();
+
+            // Calculate center positions
+            let x1 = firstBox.left + firstBox.width / 2 - container.left;
+            let y1 = firstBox.top + firstBox.height / 2 - container.top;
+
+            let x2 = lastBox.left + lastBox.width / 2 - container.left;
+            let y2 = lastBox.top + lastBox.height / 2 - container.top;
+
+            let length = Math.hypot(x2 - x1, y2 - y1);
+            let angle = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+
+            line.style.width = length + "px";
+            line.style.left = x1 + "px";
+            line.style.top = y1 + "px";
+            line.style.transform = `rotate(${angle}deg)`;
         }
-    })
-}
+    });
+};
 
 // Game Logic
 // music.play()
